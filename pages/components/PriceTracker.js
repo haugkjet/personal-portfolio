e731785a874bf1916cf026b7005215386a9b20f1
Kjetil H <haugkjet@gmail.com>
2022-04-06 20:06:25 +0200

    NextJS boilerplate

---
+++ pages/components/PriceTracker.js
@@ -0,0 +1,10 @@
+function PriceTracker (props) {
+
+    return <div className="flex">
+                <div className="text-blue-500 font-bold text-sm ml-4">{props.ticker}</div>
+                <div className="text-red-500 font-bold text-sm ml-4">{props.price}</div>
+                <div className="text-yellow-500 font-bold text-sm ml-4">{props.lastUpdate}</div>
+            </div>
+}
+
+export default PriceTracker;
