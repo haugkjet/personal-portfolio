e28c5dfcbd98f8431b9a9368253c901b52145324
Kjetil H <haugkjet@gmail.com>
2022-04-09 20:15:08 +0200

    Working on Getting assettypes using Fragment instead of div

--- pages/api/assetTypes.js
+++ pages/api/assetTypes.js
@@ -1,4 +1,12 @@
 export default function handler(req, res) {
-    res.status(200).json([{ name: 'Crypto'}, { name: 'Cash'},
-     { name: 'Stocks'}, { name: 'Mutual Fund'}, { name: 'Derivative'}], { name: 'Real Estate'})
-  }
+  res.status(200).json({
+    assetTypes: [
+      { id: "1", name: "Crypto" },
+      { id: "2", name: "Cash" },
+      { id: "3", name: "Stocks" },
+      { id: "4", name: "Mutual Fund" },
+      { id: "5", name: "Derivative" },
+      { id: "6", name: "Real Estate" },
+    ],
+  });
+}
--- pages/assetTypes.js
+++ pages/assetTypes.js
@@ -1,19 +1,51 @@
+import { Fragment, useState } from "react";
+
 import TopBar from "../components/TopBar";
 import SideBar from "../components/SideBar";
-
 import Footer from "../components/Footer";
 
-export default function assetTypes() {
+export default function fetchAssetTypes(props) {
+  const [assetTypes, setAssetTypes] = useState(props.assetTypes);
+
+  async function fetchAssetTypeData() {
+    console.log(`Fetching AssetTypes`);
+
+    const req = `/api/assetTypes`;
+    const response = await fetch(req);
+    const data = await response.json(response);
+    const res = await data;
+
+    setAssetTypes(data.assetTypes);
+    return res;
+  }
+
+  const clickHandler = () => {
+    fetchAssetTypeData();
+  };
   return (
-    <div>
+    <Fragment>
       <TopBar></TopBar>
       <div className="flex">
         <SideBar></SideBar>
         <div className="w-3/4">
           <h1 className="text-3xl text-green-500 font-bold">Asset Types</h1>
+          <button
+            onClick={clickHandler}
+            className="bg-blue-500 hover:bg-blue-700 text-sm text-white font-bold py-1 px-1 border border-blue-700 rounded w-1/4"
+          >
+            Get AssetTypes
+          </button>
+          {/*<div>
+            {assetTypes.map(function (d, idx) {
+              return <li key={idx}>{d.name}</li>;
+            })}
+          </div>
+
+          <div>{console.log(assetTypes)}</div>
+        */}
         </div>
       </div>
       <Footer></Footer>
-    </div>
+    </Fragment>
   );
 }
